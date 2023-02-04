bfedafca5188296658a6bcf463e7f53dfa8cffc0
Aftab Sidhu <Aftab180@gmail.com>
2023-02-04 13:42:32 -0500

    add three img

--- src/components/body/components/mission/index.tsx
+++ src/components/body/components/mission/index.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import mission1 from '../../../../assets/images/Mission-1.png';
+import mission2 from '../../../../assets/images/Mission-2.png';
+import mission3 from '../../../../assets/images/Mission-3.png';
 import './style.css';
 import { IProps } from './interface';
 
@@ -14,15 +17,19 @@ const Mission: React.FC<IProps> = (props) => {
                     <p className='inner1-p3'>Once homeless herself, Holly has made it her life’s mission to pay it forward to the homeless. By placing items including basic necessities and winter gear on walls within regions of poverty, the Walls of Love volunteers have been able to help an estimated 925,000 individuals. </p>
                     <a className='inner1-p4' href={'https://https://www.wallsoflove.com/impact.com'}>Learn more about our impact.</a>
                 </div>
-                <img></img>
+                <div className='img1-frame'>
+                    <img className='inner1-img' src={mission1} />
+                </div>
             </div>
 
             <div className='mission-frame2'>
-            <div className='inner2'>
+                <div className='img2-frame'>
+                    <img className='inner2-img' src={mission2} />
+                </div>
+                <div className='inner2'>
                     <p className='inner2-p1'>"I started Walls of Love because I know what it is like to be in a place where you need help and don't want to ask for  it out of fear of rejection and judgements. I want people to know that there are others that truly care. I strive to make the world better one person at a time.”</p>
                     <p className='inner2-p2'>— Holly Jackson, Founder of Walls of Love</p>
                 </div>
-                <img></img>
             </div>
 
             <div className='mission-frame3'>
@@ -34,8 +41,11 @@ const Mission: React.FC<IProps> = (props) => {
                     <p className='inner3-p4'>Our goal at Walls of Love is to continuously grow in cities throughout the United States touching the lives of as many individuals in need as we possibly can. When not busy supporting the rapidly growing organization, Holly can be found at Cleveland sporting events, concerts, spending time with family or cuddled up with a good book.</p>
                     <p className='inner3-p5'>To join Holly’s mission and become a part of the Walls of Love community of givers, contact us today.</p>
                 </div>
-                <img/>
+                <div className='img3-frame'>
+                    <img className='inner3-img' src={mission3} />
+                </div>
             </div>
+
             <div>
                 <img/>
                 <img/>
