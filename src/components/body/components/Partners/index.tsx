8109f6a547f18dbd3f2120aea2a0668e19c8c29d
Aftab Sidhu <Aftab180@gmail.com>
2023-02-16 13:09:18 -0500

    half done

--- src/components/body/components/Partners/index.tsx
+++ src/components/body/components/Partners/index.tsx
@@ -6,7 +6,99 @@ const Partners: React.FC<IProps> = (props) => {
 
     return (
         <div className='partners-frame'>
+            <div className='partners-frame1 '>
             <h2 className='partners-title '>Our Partners</h2>
+                <div className='partners-in1'>
+                    <h4 className='partners-headLine'>MEYERS, ROMAN, FRIEDBERG & LEWIS</h4>
+                    <p className='partners-p-a'>A Cleveland law firm. </p>
+                    <a className='partners-p-a' href='https://meyersroman.com/'>www.meyersroman.com</a>
+                    <h4 className='partners-headLine'>CRUISIN’ WITH THE DRENNANS</h4>
+                    <p className='partners-p-a'>Jim Drennan has been on almost 50 cruises and started Cruisin’ with the Drennans to share his knowledge and expertise with you.</p>
+                    <a className='partners-p-a' href={'https://www.cwtdtravel.com/'} >www.cwtdtravel.com</a>
+                </div>
+                <div className='partners-in2'>
+                    <h4 className='partners-headLine'>TURTLE FUR</h4>
+                    <p className='partners-p-a'>Since 1982 Turtle Fur® has been a leading headwear and accessories brand in the outdoor, snow sports and lifestyle markets. </p>
+                    <a className='partners-p-a' href='https://www.turtlefur.com/'>turtlefur.com</a>
+                    <h4 className='partners-headLine'>CRUISIN’ WITH THE DRENNANS</h4>
+                    <p className='partners-p-a'>Jim Drennan has been on almost 50 cruises and started Cruisin’ with the Drennans to share his knowledge and expertise with you.</p>
+                    <a className='partners-p-a' href={'https://www.rickysboxinggym.com/'} >www.rickysboxinggym.com</a>
+                </div>
+                <h2 className='partners-title '>Community Supporting Businesses and Entities</h2>
+                <div className='partners-in3'>
+                    <a className='partners-p-a' href={'https://www.firstcongelyria.org/'} >1st Congregational Church Elyria</a>
+                    <a className='partners-p-a' href={'https://wzakcleveland.com/'} >93.1 WZAK</a>
+                    <a className='partners-p-a' href={'https://www.cleveland19.com/'} >Action 19 News</a>
+                    <a className='partners-p-a' href={'https://www.appliedadhesives.com/'} >Adhesives.com</a>
+                    <a className='partners-p-a' >Alisha’a Cocina</a>
+                    <a className='partners-p-a' href={'https://www.aa.com/homePage.do'} >American Airlines</a>
+                    <a className='partners-p-a' href={'http://angeloslakewood.com/'} >Angelo's Pizza</a>
+                    <a className='partners-p-a' href={'https://www.autozone.com/locations/oh/cleveland/5910-memphis-ave.html'} >Autozone-Memphis Ave, Cleveland, OH </a>
+                    <a className='partners-p-a' href={'https://www.sweetiescandy.com/'} >B.A.Sweetie Candy Company</a>
+                    <a className='partners-p-a' >Bendix Commercial Vehicle Systems</a>
+                    <a className='partners-p-a' href={'https://www.erieinsurance.com/agencies/gg3183/carrion-insurance-llc'} >Carrion Insurance</a>
+                    <a className='partners-p-a' href={'https://case.edu/'} >Case Western Reserve University</a>
+                    <a className='partners-p-a' href={'https://www.cavendishbrewing.com/'} >Cavendish Brewing Company Gastonia, SC</a>
+                    <a className='partners-p-a' href={'https://www.centaurihs.com/'} >Centauri Health Solutions</a>
+                    <a className='partners-p-a' href={''} >Center For Better Living, East Cleveland, OH</a>
+                    <a className='partners-p-a' href={''} >Chain of Lakes High School Complex - PSC</a>
+                    <a className='partners-p-a' >Chris and Michelle Casto</a>
+                    <a className='partners-p-a' href={''} >Chronicle Telegram</a>
+                    <a className='partners-p-a' href={''} >Clara White Mission  Jacksonville, Fl</a>
+                    <a className='partners-p-a' href={''} >Classie Cakes by Yvonne</a>
+                    <a className='partners-p-a' href={''} >CLE Clothing Company</a>
+                    <a className='partners-p-a' href={''} >Cleveland Browns</a>
+                    <a className='partners-p-a' href={''} >Cleveland Cavaliers</a>
+                    <a className='partners-p-a' href={''} >Cleveland Central Catholic High School</a>
+                    <a className='partners-p-a' href={''} >Cleveland Indians</a>
+                    <a className='partners-p-a' href={''} >Cleveland Metropolitan School District</a>
+                    <a className='partners-p-a' href={''} >Cleveland Police Department</a>
+                    <a className='partners-p-a' href={''} >Cleveland State University</a>
+                    <a className='partners-p-a' href={''} >Cleveland's Local Eatery</a>
+                    <a className='partners-p-a' href={''} >CNN</a>
+                    <a className='partners-p-a' href={''} >Congin's Pizza</a>
+                    <a className='partners-p-a' href={''} >Community West Foundation</a>
+                    <a className='partners-p-a' href={''} >D & D Roofing</a>
+                    <a className='partners-p-a' href={''} >Domino's Pizza Vernon Odom Blvd Akron</a>
+                    <a className='partners-p-a' href={''} >Domino’s Pizza - 730 E Market St Akron</a>
+                    <a className='partners-p-a' href={''} >Don's Lighthouse</a>
+                    <a className='partners-p-a' href={''} >Doubletree Hotel- Tudor Arms</a>
+                    <a className='partners-p-a' href={''} >DoubleTree Hotel-Westlake</a>
+                    <a className='partners-p-a' href={''} >Drury Hotels</a>
+                    <a className='partners-p-a' href={''} >ETAS- Elyria</a>
+                    <a className='partners-p-a' href={''} >Fast Signs Cleveland</a>
+                    <a className='partners-p-a' href={''} >Fidelity National</a>
+                    <a className='partners-p-a' href={''} >Fifth Third Bank- Kinsman</a>
+                    <a className='partners-p-a' href={''} >Fairbanks North Star Borough School District Fairbanks, AK</a>
+                    <a className='partners-p-a' href={''} >Flowerama- Memphis Ave, Cleveland, OH</a>
+                    <a className='partners-p-a' href={''} >Forresters Financial Services. (USA)</a>
+                    <a className='partners-p-a' href={''} >FOX 46 Charlotte, NC</a> 
+                    <a className='partners-p-a' href={''} >FOX 8 News</a>
+                    <a className='partners-p-a' href={''} >Front Street Capital Inc.</a>
+                    <a className='partners-p-a' href={''} >Furniture Warehouse Ohio Mentor</a>
+                    <a className='partners-p-a' href={''} >Gabe’s Family Restaurant</a>
+                    <a className='partners-p-a' href={''} >Gaston Gazette. Gastonia, SC</a>
+                    <a className='partners-p-a' href={''} >Gastonia, SC Mayor's Office</a>
+                    <a className='partners-p-a' href={''} >Get Go  Lorain Rd Fairview Park, OH</a>
+                    <a className='partners-p-a' href={''} >Giant Eagle Marketplace- Solon</a>
+                    <a className='partners-p-a' href={''} >Grace Hope Presbyterian Church</a>
+                    <a className='partners-p-a' href={''} >Human Arc</a> 
+                    <a className='partners-p-a' href={''} >Houston First Baptist Church  Houston, Tx</a>
+                    <a className='partners-p-a' href={''} >ISH’s Property Maintenance</a>
+                    <a className='partners-p-a' href={''} >Infinity Title Agency. Ohio</a>
+                    <a className='partners-p-a' href={''} ></a>
+                    <a className='partners-p-a' href={''} ></a>
+                    <a className='partners-p-a' href={''} ></a>
+                    <a className='partners-p-a' href={''} ></a>
+                    <a className='partners-p-a' href={''} ></a>
+                    <a className='partners-p-a' href={''} ></a>
+                    <a className='partners-p-a' href={''} ></a> 
+                    <a className='partners-p-a' href={''} ></a>
+                    <a className='partners-p-a' href={''} ></a>
+                    <a className='partners-p-a' href={''} ></a>
+                </div>
+            </div>
+
         </div>
     )
 }
