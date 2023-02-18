0e0c903d7d90e43e87272fcbed54b63d18e62c40
Aftab Sidhu <Aftab180@gmail.com>
2023-02-18 13:27:35 -0500

    done

--- src/components/body/components/Partners/index.tsx
+++ src/components/body/components/Partners/index.tsx
@@ -40,62 +40,118 @@ const Partners: React.FC<IProps> = (props) => {
                     <a className='partners-p-a' href={'https://case.edu/'} >Case Western Reserve University</a>
                     <a className='partners-p-a' href={'https://www.cavendishbrewing.com/'} >Cavendish Brewing Company Gastonia, SC</a>
                     <a className='partners-p-a' href={'https://www.centaurihs.com/'} >Centauri Health Solutions</a>
-                    <a className='partners-p-a' href={''} >Center For Better Living, East Cleveland, OH</a>
-                    <a className='partners-p-a' href={''} >Chain of Lakes High School Complex - PSC</a>
+                    <a className='partners-p-a' href={'https://livingtruthcenter1.weebly.com/'} >Center For Better Living, East Cleveland, OH</a>
+                    <a className='partners-p-a' href={'https://www.polk.edu/chain-of-lakes-collegiate-high-school/'} >Chain of Lakes High School Complex - PSC</a>
                     <a className='partners-p-a' >Chris and Michelle Casto</a>
-                    <a className='partners-p-a' href={''} >Chronicle Telegram</a>
-                    <a className='partners-p-a' href={''} >Clara White Mission  Jacksonville, Fl</a>
-                    <a className='partners-p-a' href={''} >Classie Cakes by Yvonne</a>
-                    <a className='partners-p-a' href={''} >CLE Clothing Company</a>
-                    <a className='partners-p-a' href={''} >Cleveland Browns</a>
-                    <a className='partners-p-a' href={''} >Cleveland Cavaliers</a>
-                    <a className='partners-p-a' href={''} >Cleveland Central Catholic High School</a>
-                    <a className='partners-p-a' href={''} >Cleveland Indians</a>
-                    <a className='partners-p-a' href={''} >Cleveland Metropolitan School District</a>
-                    <a className='partners-p-a' href={''} >Cleveland Police Department</a>
-                    <a className='partners-p-a' href={''} >Cleveland State University</a>
-                    <a className='partners-p-a' href={''} >Cleveland's Local Eatery</a>
-                    <a className='partners-p-a' href={''} >CNN</a>
-                    <a className='partners-p-a' href={''} >Congin's Pizza</a>
-                    <a className='partners-p-a' href={''} >Community West Foundation</a>
-                    <a className='partners-p-a' href={''} >D & D Roofing</a>
-                    <a className='partners-p-a' href={''} >Domino's Pizza Vernon Odom Blvd Akron</a>
-                    <a className='partners-p-a' href={''} >Domino’s Pizza - 730 E Market St Akron</a>
-                    <a className='partners-p-a' href={''} >Don's Lighthouse</a>
-                    <a className='partners-p-a' href={''} >Doubletree Hotel- Tudor Arms</a>
-                    <a className='partners-p-a' href={''} >DoubleTree Hotel-Westlake</a>
-                    <a className='partners-p-a' href={''} >Drury Hotels</a>
-                    <a className='partners-p-a' href={''} >ETAS- Elyria</a>
-                    <a className='partners-p-a' href={''} >Fast Signs Cleveland</a>
-                    <a className='partners-p-a' href={''} >Fidelity National</a>
-                    <a className='partners-p-a' href={''} >Fifth Third Bank- Kinsman</a>
-                    <a className='partners-p-a' href={''} >Fairbanks North Star Borough School District Fairbanks, AK</a>
-                    <a className='partners-p-a' href={''} >Flowerama- Memphis Ave, Cleveland, OH</a>
-                    <a className='partners-p-a' href={''} >Forresters Financial Services. (USA)</a>
-                    <a className='partners-p-a' href={''} >FOX 46 Charlotte, NC</a> 
-                    <a className='partners-p-a' href={''} >FOX 8 News</a>
-                    <a className='partners-p-a' href={''} >Front Street Capital Inc.</a>
-                    <a className='partners-p-a' href={''} >Furniture Warehouse Ohio Mentor</a>
-                    <a className='partners-p-a' href={''} >Gabe’s Family Restaurant</a>
-                    <a className='partners-p-a' href={''} >Gaston Gazette. Gastonia, SC</a>
-                    <a className='partners-p-a' href={''} >Gastonia, SC Mayor's Office</a>
-                    <a className='partners-p-a' href={''} >Get Go  Lorain Rd Fairview Park, OH</a>
-                    <a className='partners-p-a' href={''} >Giant Eagle Marketplace- Solon</a>
-                    <a className='partners-p-a' href={''} >Grace Hope Presbyterian Church</a>
-                    <a className='partners-p-a' href={''} >Human Arc</a> 
-                    <a className='partners-p-a' href={''} >Houston First Baptist Church  Houston, Tx</a>
-                    <a className='partners-p-a' href={''} >ISH’s Property Maintenance</a>
-                    <a className='partners-p-a' href={''} >Infinity Title Agency. Ohio</a>
+                    <a className='partners-p-a' href={'https://chroniclet.com/'} >Chronicle Telegram</a>
+                    <a className='partners-p-a' href={'https://www.clarawhitemission.org/'} >Clara White Mission  Jacksonville, Fl</a>
+                    <a className='partners-p-a' href={'https://www.facebook.com/ClassieCakesByYvonne/'} >Classie Cakes by Yvonne</a>
+                    <a className='partners-p-a' href={'CLE Clothing Company'} >CLE Clothing Company</a>
+                    <a className='partners-p-a' href={'https://www.clevelandbrowns.com/'} >Cleveland Browns</a>
+                    <a className='partners-p-a' href={'https://www.nba.com/cavaliers'} >Cleveland Cavaliers</a>
+                    <a className='partners-p-a' href={'https://www.centralcatholichs.org/'} >Cleveland Central Catholic High School</a>
+                    <a className='partners-p-a' href={'https://www.mlb.com/guardians'} >Cleveland Indians</a>
+                    <a className='partners-p-a' href={'https://www.clevelandmetroschools.org/'} >Cleveland Metropolitan School District</a>
+                    <a className='partners-p-a' href={'https://www.clevelandohio.gov/CityofCleveland/Home/Government/CityAgencies/PublicSafety/Police'} >Cleveland Police Department</a>
+                    <a className='partners-p-a' href={'https://www.csuohio.edu/'} >Cleveland State University</a>
+                    <a className='partners-p-a' href={'https://www.facebook.com/pages/Clevelands-Local-Eatery/261380747848348'} >Cleveland's Local Eatery</a>
+                    <a className='partners-p-a' href={'https://www.cnn.com/'} >CNN</a>
+                    <a className='partners-p-a' href={'https://conginspizza.com/'} >Congin's Pizza</a>
+                    <a className='partners-p-a' href={'https://www.communitywestfoundation.org/'} >Community West Foundation</a>
+                    <a className='partners-p-a' href={'https://www.angi.com/companylist/us/oh/independence/dandd-roofing-reviews-5540142.htm'} >D & D Roofing</a>
+                    <a className='partners-p-a' href={' https://www.dominos.com/en/?redirect=homepage&utm_campaign=localmaps&utm_medium=loclist&utm_source=google'} >Domino's Pizza Vernon Odom Blvd Akron</a>
+                    <a className='partners-p-a' href={'https://www.dominos.com/en/?utm_campaign=localmaps&utm_medium=loclist&utm_source=google'} >Domino’s Pizza - 730 E Market St Akron</a>
+                    <a className='partners-p-a' href={'https://donslighthouse.com/'} >Don's Lighthouse</a>
+                    <a className='partners-p-a' href={'https://www.hilton.com/en/hotels/cleucdt-the-tudor-arms-hotel-cleveland/'} >Doubletree Hotel- Tudor Arms</a>
+                    <a className='partners-p-a' href={'https://www.hilton.com/en/hotels/clecrdt-doubletree-cleveland-westlake/?SEO_id=GMB-DT-CLECRDT'} >DoubleTree Hotel-Westlake</a>
+                    <a className='partners-p-a' href={'https://www.druryhotels.com/'} >Drury Hotels</a>
+                    <a className='partners-p-a' href={'https://www.socelyria.com/elyria-teens-achieve-success-etas.html'} >ETAS- Elyria</a>
+                    <a className='partners-p-a' href={'https://www.fastsigns.com/downtown-cleveland-oh/?utm_source=google&utm_medium=local&utm_campaign=localmaps&utm_content=333'} >Fast Signs Cleveland</a>
+                    <a className='partners-p-a' href={'https://fnf.com/'} >Fidelity National</a>
+                    <a className='partners-p-a' href={'https://www.53.com/content/fifth-third/en.html'} >Fifth Third Bank- Kinsman</a>
+                    <a className='partners-p-a' href={'https://www.k12northstar.org/'} >Fairbanks North Star Borough School District Fairbanks, AK</a>
+                    <a className='partners-p-a' href={'https://www.yelp.com/biz/flowerama-cleveland-3'} >Flowerama- Memphis Ave, Cleveland, OH</a>
+                    <a className='partners-p-a' href={'https://www.forrester.com/bold'} >Forresters Financial Services. (USA)</a>
+                    <a className='partners-p-a' href={'https://www.qcnews.com/'} >FOX 46 Charlotte, NC</a> 
+                    <a className='partners-p-a' href={'https://fox8.com/'} >FOX 8 News</a>
+                    <a className='partners-p-a' href={'https://frontstreetcapitalnc.com/'} >Front Street Capital Inc.</a>
+                    <a className='partners-p-a' href={'https://fwohio.com/?v=7516fd43adaa'} >Furniture Warehouse Ohio Mentor</a>
+                    <a className='partners-p-a' href={'https://www.tripadvisor.com/Restaurant_Review-g50207-d4947982-Reviews-Gabe_s_Family_Restaurant-Cleveland_Ohio.html'} >Gabe’s Family Restaurant</a>
+                    <a className='partners-p-a' href={'https://www.gastongazette.com/'} >Gaston Gazette. Gastonia, SC</a>
+                    <a className='partners-p-a' href={'https://www.cityofgastonia.com/'} >Gastonia, SC Mayor's Office</a>
+                    <a className='partners-p-a' href={'https://www.gianteagle.com/'} >Get Go  Lorain Rd Fairview Park, OH</a>
+                    <a className='partners-p-a' href={'https://www.gianteagle.com/'} >Giant Eagle Marketplace- Solon</a>
+                    <a className='partners-p-a' href={'https://wearegracehope.com/'} >Grace Hope Presbyterian Church</a>
+                    <a className='partners-p-a' href={'https://www.centaurihs.com/'} >Human Arc</a> 
+                    <a className='partners-p-a' href={'https://houstonsfirst.org/'} >Houston First Baptist Church  Houston, Tx</a>
+                    <a className='partners-p-a' href={'https://www.facebook.com/profile.php?id=100063524351877'} >ISH’s Property Maintenance</a>
+                    <a className='partners-p-a' href={'https://www.infinitytitleagency.com/'} >Infinity Title Agency. Ohio</a>
                     <a className='partners-p-a' href={''} ></a>
-                    <a className='partners-p-a' href={''} ></a>
-                    <a className='partners-p-a' href={''} ></a>
-                    <a className='partners-p-a' href={''} ></a>
-                    <a className='partners-p-a' href={''} ></a>
-                    <a className='partners-p-a' href={''} ></a>
-                    <a className='partners-p-a' href={''} ></a> 
-                    <a className='partners-p-a' href={''} ></a>
-                    <a className='partners-p-a' href={''} ></a>
-                    <a className='partners-p-a' href={''} ></a>
+                    <p className='partners-p-a' ></p>
+                </div>
+                <div className='partners-in4'>
+                <p className='partners-p-a' >Jill Riegelmayer</p>
+                <a className='partners-p-a' href={'https://www.drivejoyce.com/'} >Joyce Buick</a>
+                <a className='partners-p-a' href={'https://www.seniorhousingnet.com/seniorliving-detail/kappa-house-ii-apartments_12300-shaker-blvd_cleveland_oh_44147-602058'} >Kappa House I and II</a>
+                <a className='partners-p-a' href={'https://littlecaesars.com/en-us/store/8730'} >Little Caesars Pizza- Broad Street, Elyria, OH (Franchise)</a>
+                <a className='partners-p-a' href={'https://littlecaesars.com/en-us/store/1420'} >Little Caesars Pizza- Broadway Ave, Lorain, OH (Franchise)</a>
+                <a className='partners-p-a' href={'https://littlecaesars.com/en-us/store/7993'} >Little Caesars Pizza- Buckeye Ave, Cleveland, OH (Franchise)</a>
+                <a className='partners-p-a' href={'https://littlecaesars.com/en-us/store/8259'} >Little Caesars Pizza- Clark Avenue, Cleveland, OH (Franchise)</a>
+                <a className='partners-p-a' href={'https://www.facebook.com/profile.php?id=100063738092648'} >Lone Tree Tavern. North Ridgeville, Ohio</a>
+                <a className='partners-p-a' href={'https://unionhall.aflcio.org/loraincountyclc'} >Lorain County AFL-CIO</a>
+                <p className='partners-p-a' >Marilyn Werner</p>
+                <a className='partners-p-a' href={'https://maryqop.org/'} >Mary Queen of Peace Church</a>
+                <a className='partners-p-a' href={'https://www.mcdonalds.com/us/en-us.html'} >McDonalds- Henderson Ave, Lorain, OH (Franchise)</a>
+                <a className='partners-p-a' href={'https://www.mcdonalds.com/us/en-us/location/oh/lorain/3221-oberlin-ave/16255.html'} >McDonalds- Oberlin Ave, Lorain, OH (Franchise)</a>
+                <a className='partners-p-a' href={'https://www.facebook.com/metropolistattoo216/'} >Metropolis Tattoo on Clark Ave.</a>
+                <a className='partners-p-a' href={'https://www.newellbrands.com/'} >Newell Brands</a>
+                <a className='partners-p-a' href={'https://nnoble.com/'} >Norman Noble, Inc.</a>
+                <a className='partners-p-a' href={'https://locations.oreillyauto.com/oh/cleveland/autoparts-3999.html'} >O'Reily Auto Parts- Fulton Ave, Cleveland, OH</a>
+                <a className='partners-p-a' href={'https://www.officedepot.com/'} >Office Max -Kent</a>
+                <a className='partners-p-a' href={'https://www.officedepot.com/'} >Office Max- AkronOffice Max- North Olmstead</a>
+                <a className='partners-p-a' href={'https://www.officedepot.com/'} >Office Max- Parma</a>
+                <a className='partners-p-a' href={'https://www.officedepot.com/'} >Office Max- Youngstown</a>
+                <p className='partners-p-a' >Papa Joe's Pizza</p>
+                <a className='partners-p-a' href={'https://www.unitedchurchhomes.org/parkvue/'} >Parkview Community Sandusky, Ohio</a>
+                <a className='partners-p-a' href={'https://paparazziaccessories.com/217691/'} >Paparazzi- Lisa Zimmerman</a>
+                <a className='partners-p-a' href={'https://pierw.com/'} >Pier W Positive Education Program</a>
+                <a className='partners-p-a' href={'https://www.polk.edu/?utm_campaign=yext&utm_content=winter_haven&utm_medium=organic&utm_source=local'} >Polk State College</a>
+                <a className='partners-p-a' href={'http://www.rakenow.org/'} >R.A.K.E. (Random Acts of Kindness Everywhere)</a>
+                <a className='partners-p-a' href={'https://robertos-kids.org/'} >Roberto’s Kids Foundation</a>
+                <a className='partners-p-a' href={'https://www.record-courier.com/'} >Record Courier</a>
+                <a className='partners-p-a' href={'https://ridgepark-square.com/error404'} >Rockne's-Ridge Park Square</a>
+                <a className='partners-p-a' href={'https://www.saladkraze.com/'} >Salad Kraze Avon Lake</a>
+                <a className='partners-p-a' href={'https://www.socelyria.com/programs.html'} >Save Our Children Elyria</a>
+                <a className='partners-p-a' href={'https://sacredheartsi.org/'} >Sacred Heart Church. Staten Island, NY</a>
+                <p className='partners-p-a' >Shoreway Sports and Graphics Lorain, Ohio</p>
+                <p className='partners-p-a' >Smack Yo Mamma</p>
+                <a className='partners-p-a' href={'http://www.saintjohnucc.com/'} >St. John United Church of Christ</a>
+                <a className='partners-p-a' href={'https://statusinkllc.com/'} >Status Ink LLC</a>
+                <a className='partners-p-a' href={'https://www.suburbantemple.org/'} >Suburban Temple- Kol Ami</a>
+                <a className='partners-p-a' href={'https://www.subway.com/en-us/'} >Subway-Ridge Rd Elyria</a>
+                <a className='partners-p-a' href={'https://spectrumlocalnews.com/splash'} >Spectrum 1 News</a>
+                <a className='partners-p-a' href={'https://stmaryavon.org/'} >St Mary Church PSR Avon, OH</a>
+                <a className='partners-p-a' href={'https://www.texasroadhouse.com/'} >Texas Roadhouse- Ridge Park Square</a>
+                <p className='partners-p-a' >Tim Carrion Insurance Lorain, Ohio</p>
+                <a className='partners-p-a' href={'https://ecofohio.com/'} >The Electrolizing Corporation of Ohio</a>
+                <a className='partners-p-a' href={'https://www.facebook.com/pages/The-Hair-Gallery/132077926836737'} >The Hair Gallery-Patty Huckleberry</a>
+                <a className='partners-p-a' href={'https://www.morningjournal.com/'} >The Morning Journal</a>
+                <a className='partners-p-a' href={'https://transitionssc.org/'} >Transitions Homeless Center Columbia, SC</a>
+                <a className='partners-p-a' href={'https://www.trials4hope.org/'} >Trials For Hope, Cleveland, OH</a>
+                <a className='partners-p-a' href={'https://www.trinitycleveland.org/'} >Trinity Cathedral Episcopal Church</a>
+                <a className='partners-p-a' href={'https://www.triplepeaksroofing.com/'} >Triple Peaks Roofing</a>
+                <a className='partners-p-a' href={'https://www.uhhospitals.org/'} >UH Tri Family Medicine</a>
+                <a className='partners-p-a' href={'https://www.usy.org/'} >United Synagogue Youth</a>
+                <a className='partners-p-a' href={'https://www.uakron.edu/'} >University of Akron</a>
+                <p className='partners-p-a' >Urban Farmer</p>
+                <a className='partners-p-a' href={'https://www.vfw.org/'} >VFW- W 61 St. Cleveland</a>
+                <a className='partners-p-a' href={'https://www.vmsinc.com/'} >Visual Marketing Services-Twinsburg</a>
+                <a className='partners-p-a' href={'https://www.news5cleveland.com/'} >WEWS Channel 5 News</a>
+                <a className='partners-p-a' href={'https://www.wistv.com/'} >WIS News SC</a>
+                <a className='partners-p-a' href={'https://www.wkyc.com/'} >WKYC Channel 3</a>
+                <a className='partners-p-a' href={'https://zphib1920.org/'} >Zeta Phi Beta Sorority, Incorporated</a>
+                <a className='partners-p-a' href={''} ></a>
+                <p className='partners-p-a' ></p>
                 </div>
             </div>
 
