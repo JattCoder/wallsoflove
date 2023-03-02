1bbe2b2d165b1eb13bcf644a1bc7252b12390bd4
Aftab Sidhu <Aftab180@gmail.com>
2023-03-01 22:00:45 -0500

    first

--- src/components/body/components/Blog/index.tsx
+++ src/components/body/components/Blog/index.tsx
@@ -19,7 +19,7 @@ const Blog: React.FC<IProps> = (props) => {
                     <p className='blog-dates'>August 29, 2021</p>
                     <a className='blog-headLine'>Walls of Love Partners with the Cleveland Indians to do its EPIC 1000th Wall in Cleveland</a>
                     <p className='blog-subtitle'>Event provides essentials, school supplies and hygiene supplies to those in need.</p>
-                    <a className='blog-subtitle'>READ MORE ....</a>
+                    <a className='blog-subtitle1'>READ MORE ....</a>
                 </div>
                 <div className='blog-rightHalf'>
                     <img className='blog-img' src={blog2}/>
@@ -33,7 +33,7 @@ const Blog: React.FC<IProps> = (props) => {
                     <p className='blog-dates'>April 27, 2021</p>
                     <a className='blog-headLine'>Walls of Love is Putting Up their 1000th Wall | Why should we come to your city?</a>
                     <p className='blog-subtitle'>Walls of Love is coming up on their 1000th wall and needs to know where it should be placed! Starting April 28th, we will be accepting entries as to why we should come to your city to put up our 1000th wall. We will stop taking entries on May 31, 2021 and announce the winning city on June 14, 2021. Entries can be sent via email to wallsoflove1@gmail.com.</p>
-                    <a className='blog-subtitle'>READ MORE ....</a>
+                    <a className='blog-subtitle1'>READ MORE ....</a>
                 </div>
                 <div className='blog-rightHalf'>
                     <img className='blog-img' src={blog4}/>
@@ -47,14 +47,14 @@ const Blog: React.FC<IProps> = (props) => {
                     <p className='blog-dates'>July 6, 2019</p>
                     <a className='blog-headLine'>Walls of Love T-Shirts Now Available</a>
                     <p className='blog-subtitle'>100% of the proceeds from these T-shirts goes directly back to those in need.</p>
-                    <a className='blog-subtitle'>READ MORE ....</a>
+                    <a className='blog-subtitle1'>READ MORE ....</a>
                 </div>
                 <div className='blog-rightHalf'>
                     <img className='blog-img' src={blog6}/>
                     <p className='blog-dates'>May 9, 2018</p>
                     <a className='blog-headLine'>100th Wall Featured on Channel 19 News</a>
                     <p className='blog-subtitle'>This is an example blog post excerpt.</p>
-                    <a className='blog-subtitle'>READ MORE ....</a>
+                    <a className='blog-subtitle1'>READ MORE ....</a>
                 </div>
             </div>
             <div className='blog-frame1'>
@@ -63,14 +63,14 @@ const Blog: React.FC<IProps> = (props) => {
                     <p className='blog-dates'></p>
                     <a className='blog-headLine'></a>
                     <p className='blog-subtitle'></p>
-                    <a className='blog-subtitle'></a>
+                    <a className='blog-subtitle1'></a>
                 </div>
                 <div className='blog-rightHalf'>
                     <img className='blog-img' src={''}/>
                     <p className='blog-dates'></p>
                     <a className='blog-headLine'></a>
                     <p className='blog-subtitle'></p>
-                    <a className='blog-subtitle'></a>
+                    <a className='blog-subtitle1'></a>
                 </div>
             </div>
         </div>
--- src/components/bottom/index.tsx
+++ src/components/bottom/index.tsx
@@ -5,7 +5,33 @@ const Bottom = () => {
 
     return (
         <div className='bottom-frame'>
-
+            <div className='bottom-frame1'>
+                <div className='bottom-inner1'>
+                    <p>© 2018 WALLS OF LOVE{'\n'}{'\n'}Cleveland, Ohio{'\n'}{'\n'}(216) 882-9395</p>
+                    <a>wallsoflove216@gmail.com</a>
+                    <p>{'\n'}Walls of Love® is a registered trademark{'\n'}and cannot be reused without expressed{'\n'}written permission. All rights are reserved.</p>
+                </div>
+                <div className='bottom-inner2'>
+                    <p className='bottom-p'>INFO</p>
+                    <a className='bottom-a'>Mission</a>
+                    <a className='bottom-a'>Partners</a>
+                    <a className='bottom-a'>Impact</a>
+                    <a className='bottom-a'>Upcoming Events</a>
+                    <a className='bottom-a'>Press</a>
+                    <a className='bottom-a'>Blag</a>
+                </div>
+                <div className='bottom-inner2'>
+                    <p className='bottom-p'>ACTION</p>
+                    <a className='bottom-a'>Volunteer</a>
+                    <a className='bottom-a'>Contact</a>
+                    <a className='bottom-a'>Donate</a>
+                </div>
+                <div className='bottom-inner1'>
+                    <p>Sign up to receive news and updates.</p>
+                    <input className='bottom-input' placeholder='Email Address'/>
+                    <input type="submit" value="Submit" className='bottom-submit'></input>
+                </div>
+            </div>
         </div>
     )
 }
